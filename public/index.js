89827d0fefc5b405bcf8f3eef24252f9ad5fcd91
bennyKi <76220769+bennyKi@users.noreply.github.com>
2021-04-03 16:27:13 +0200

    beheben des level-fehlers

--- public/index.js
+++ public/index.js
@@ -414,7 +414,7 @@ window.onload = async function () {
                 if (collision) {
                     console.log("collision", collision, "compare", compare(self.type, player.type));
                     if (compare(self.type, player.type) && compare(self.type, player.type) != "draw") {
-                        if (self.level < 4) {
+                        if (self.level < 3) {
                             self.level++;
                         }
                         console.log(self.level);
